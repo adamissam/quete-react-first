40ab555a1e9655bfad48159ee621321a7796226f
Adam Issam <issamadam15@gmail.com>
2019-10-06 19:25:57 +0200

    creation of QuoteCard in js and css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 import Navbar from './componement/Navbar';
+import QuoteCard from './componement/QuoteCard'
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <QuoteCard />
     </div>
   );
 }
