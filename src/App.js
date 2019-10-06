664537a28680dc463c1bf55a3c660f06d07d7860
Adam Issam <issamadam15@gmail.com>
2019-10-06 18:54:03 +0200

    creat navbar function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
+import Navbar from './componement/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
---
+++ src/componement/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Navbar.css'
+
+function Navbar(){
+    return <nav className = 'Navbar'>
+        <h1>Toni Montana</h1>
+        <p>La tronche de qui</p>
+    </nav>
+}
+
+export default Navbar;
